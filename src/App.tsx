c9b5e4ebf388643fd23a666c4b4e1a328ffc3df9
dmm <dmm@anadeainc.com>
2021-11-09 23:11:19 +0300

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
+import AppRouter from "./components/AppRouter";
+import NavBar from "./components/NavBar";
+import {Layout} from "antd";
+import "./App.css"
+import {useActions} from "./hooks/useActions";
+import {IUser} from "./models/IUser";
 
 const App: FC = () => {
+
+    const {setUser, setIsAuth} = useActions()
+
+    useEffect(() => {
+        if(localStorage.getItem("auth")){
+            setUser({username: localStorage.getItem("username" || "")} as IUser)
+            setIsAuth(true)
+        }
+    }, [])
+
     return (
-        <div>
-            App
-        </div>
+        <Layout>
+            <NavBar/>
+            <Layout.Content>
+                <AppRouter/>
+            </Layout.Content>
+        </Layout>
     );
 };
 
---
+++ src/utils/date.ts
@@ -0,0 +1,9 @@
+import {Moment} from "moment";
+
+
+export const formatDate = (date: Date): string => {
+    const year = date.getFullYear()
+    const month = date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1
+    const day = date.getDay() < 10 ? `0${date.getDay() + 1}` : date.getDay() + 1
+    return `${year}.${month}.${day}`
+}
